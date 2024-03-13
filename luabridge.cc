636283559c8415090eff8c492a48b49eb835f169
Job Snijders <job@sobornost.net>
2024-03-13 22:19:08 +0000

    Add configuration options to set Ntfy URL and Authorization header

--- luabridge.cc
+++ luabridge.cc
@@ -105,7 +105,7 @@ void initLua()
   g_lua.set_function("ntfyNotifier", [&](sol::table data) {
     checkLuaTable(data, {"topic"});
     g_notifiers.emplace_back(
-                             make_shared<NtfyNotifier>(data.get<string>("topic")));
+                             make_shared<NtfyNotifier>(data));
     return *g_notifiers.rbegin();
   });
 

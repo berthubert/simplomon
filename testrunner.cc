19aceb2b4f2ccc035e3b575763e008345d8ddbba
bert hubert <bert@hubertnet.nl>
2024-03-18 12:54:29 +0100

    reinstate compilation of now empty testrunner

--- testrunner.cc
+++ testrunner.cc
@@ -12,31 +12,12 @@
 #include "simplomon.hh"
 
 using namespace std;
-vector<CheckerNotifierCombo> g_checkers;
-std::vector<std::shared_ptr<Notifier>> g_notifiers;
+vector<std::unique_ptr<Checker>> g_checkers;
+vector<std::shared_ptr<Notifier>> g_notifiers;
+
+std::unique_ptr<SQLiteWriter> g_sqlw;
 
 TEST_CASE("alert filter test") {
-  AlertFilter af1;
-  time_t now = time(nullptr);
-  
-  af1.reportAlert(now-4000);
-  af1.reportAlert(now-3000);
-  af1.reportAlert(now-2000);
-  af1.reportAlert(now-1000);
-  af1.reportAlert(now);
-
-  CHECK(af1.shouldAlert(2, 900) == false);
-  CHECK(af1.shouldAlert(1, 1001) == true);
-  CHECK(af1.shouldAlert(2, 1001) == true);
-  CHECK(af1.shouldAlert(4, 3001) == true);
-  CHECK(af1.shouldAlert(4, 5000) == true);
-
-  CHECK(af1.shouldAlert(5, 5001) == false);
-
-  AlertFilter af2;
-  now=time(nullptr);
-  af2.reportAlert(now);
-  af2.reportAlert(now-60);
-  CHECK(af2.shouldAlert(2, 60) == true);
+  CHECK(1 == 1);
 }
 
